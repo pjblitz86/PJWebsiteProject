3d12f4914f1d3d0713e361b739c2da04bc981e74
Paulius Juknis <paulius.juknis@gmail.com>
2018-04-19 13:55:49 +0300

    initial commit

--- PJWebsiteProject/Data/Migrations/20180419105034_Initial.Designer.cs
+++ PJWebsiteProject/Data/Migrations/20180419105034_Initial.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace PJWebsiteProject.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180419105034_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
