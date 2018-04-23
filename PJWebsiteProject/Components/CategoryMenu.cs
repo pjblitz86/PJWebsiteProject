955778a27b1e91e32783bba1a125d59a2643574b
Paulius Juknis <paulius.juknis@gmail.com>
2018-04-23 09:28:28 +0300

    added drinks nav dropdown and categorized drinks

---
+++ PJWebsiteProject/Components/CategoryMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PJWebsiteProject.Models.DrinkModels;
+
+namespace PJWebsiteProject.Components
+{
+	public class CategoryMenu : ViewComponent
+	{
+		private readonly IDrinkCategoryRepository _drinkCategoryRepository;
+
+		public CategoryMenu(IDrinkCategoryRepository drinkCategoryRepository)
+		{
+			_drinkCategoryRepository = drinkCategoryRepository;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			var categories = _drinkCategoryRepository.DrinkCategories.OrderBy(p => p.CategoryName);
+			return View(categories);
+		}
+	}
+}
