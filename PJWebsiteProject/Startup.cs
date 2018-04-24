4e44e440f92d1d0d2f2fa9efc831fedabfe52ff7
Paulius Juknis <paulius.juknis@gmail.com>
2018-04-24 16:48:36 +0300

    can now click on drink and go to detailed page

--- PJWebsiteProject/Startup.cs
+++ PJWebsiteProject/Startup.cs
@@ -81,8 +81,19 @@ namespace PJWebsiteProject
 			//			app.UseMvcWithDefaultRoute();
 			app.UseMvc(routes =>
 			{
-				routes.MapRoute(name: "categoryFilter", template: "Drink/{action}/{category?}", defaults: new { Controller = "Drink", action = "List" });
-				routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
+				routes.MapRoute(
+					name: "drinkdetails",
+					template: "Drink/Details/{drinkId?}",
+					defaults: new { Controller = "Drink", action = "Details" });
+
+				routes.MapRoute(
+					name: "categoryFilter",
+					template: "Drink/{action}/{category?}",
+					defaults: new { Controller = "Drink", action = "List" });
+
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
 			});
 
 		}
